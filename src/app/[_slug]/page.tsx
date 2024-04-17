339d32a2f0126dea297b0d86e44072df4d0d6884
qashifpeer <mail.qashifpeer@gmail.com>
2024-04-17 20:00:31 +0530

    rf:heading styled

--- src/app/[_slug]/page.tsx
+++ src/app/[_slug]/page.tsx
@@ -35,15 +35,16 @@ async function getSinglePost(slug: string){
 
 const BlogPost = async ({ params }: { params: { _slug: string } }) => {
     const data = await getSinglePost(params._slug);
-    console.log("content_type",typeof(data.content))
+    // console.log("content_type",typeof(data.content))
 //   const postTitle = params._slug;
 //   const allPosts = await getData();
 //   const singlePost = allPosts.find((post) => postTitle === post.slug);
     // console.log("newdata",data)
   return (
     <div className="max-w-3xl mx-auto">
+        
+        <h1 className="text-3xl font-extrabold mb-4">{data.title}</h1>
         <p>{data.author.name}</p>
-        <h1>{data.title}</h1>
         <Image src={data.image} priority alt="feature Image" width={800} height={800} />
         <div className="mt-16 prose prose-lg prose-li:marker:text-primary">
         <PortableText value ={data.content} />
