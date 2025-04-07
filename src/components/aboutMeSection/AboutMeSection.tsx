225945cc6c482e5fe76f447af3affdc918ae9c13
qashifpeer <mail.qashifpeer@gmail.com>
2025-04-07 18:23:52 +0530

    escape chars fixed

--- src/components/aboutMeSection/AboutMeSection.tsx
+++ src/components/aboutMeSection/AboutMeSection.tsx
@@ -1,15 +1,22 @@
 "use client";
 
-
 const AboutMeSection = () => {
   return (
     <section className="bg-white py-12 px-6 md:px-12 lg:px-24">
       <div className="max-w-4xl mx-auto text-center">
         <h2 className="text-3xl font-bold text-gray-800 mb-6">About Us</h2>
         <p className="text-gray-600 text-lg leading-relaxed mb-6">
-        Welcome to NuzhaKashmir.com, your go-to travel partner for discovering Kashmir's stunning natural beauty. Our team of enthusiastic local travelers and storytellers is committed to letting you explore the best places to visit in Kashmir using in-depth travel guides and first-hand accounts to highlight Kashmir's rich cultural heritage, breathtaking scenery, and undiscovered treasures.
-At Nuzha Kashmir, we think that travel is about experiencing a place's soul rather than merely seeing new places. Our blog provides helpful travel advice, carefully chosen itineraries, and comprehensive destination recommendations to make your trip-planning easy, whether you're organizing a family vacation, a honeymoon, or a single trip.
-
+          Welcome to NuzhaKashmir.com, your go-to travel partner for discovering
+          Kashmir&#39;s stunning natural beauty. Our team of enthusiastic local
+          travelers and storytellers is committed to letting you explore the
+          best places to visit in Kashmir using in-depth travel guides and
+          first-hand accounts to highlight Kashmir&#39;s rich cultural heritage,
+          breathtaking scenery, and undiscovered treasures. At Nuzha Kashmir, we
+          think that travel is about experiencing a place&#39;s soul rather than
+          merely seeing new places. Our blog provides helpful travel advice,
+          carefully chosen itineraries, and comprehensive destination
+          recommendations to make your trip-planning easy, whether you are
+          organizing a family vacation, a honeymoon, or a single trip.
         </p>
         <a href="https://nuzhakashmir.com/about" target="blank">
           <a className="inline-block mt-6 px-8 py-3 bg-orange-600 hover:bg-orange-700 text-white rounded-full text-lg uppercase transition duration-300">
